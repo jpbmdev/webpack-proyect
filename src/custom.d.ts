c0139be70ba8185d7fffa1385b33571a49c16760
jpbmdev <jpbmdev@gmail.com>
2021-08-12 17:16:26 -0500

    :art: css and image modules created

---
+++ src/custom.d.ts
@@ -0,0 +1,10 @@
+declare module '*.css'{
+    const css: {[key: string]: string};
+    export default css;
+}
+
+declare module '*.svg' {
+    const ReactComponent: React.ComponentType<React.SVGAttributes<SVGAElement>>;
+    export default ReactComponent;
+
+}
